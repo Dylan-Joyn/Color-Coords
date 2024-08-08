8bede9e01bcce2168458cca600e4cda25694ec35
Dylan-Joyn <jdjjoyner@gmail.com>
2024-08-08 11:51:39 -0500

    update

--- Color Coords/Color Coords.cpp
+++ Color Coords/Color Coords.cpp
@@ -1,12 +1,28 @@
 // Color Coords.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <fstream>
 #include <iostream>
+#include <string>
+#include "Color.h"
+#include "Picture.h"
+#include "FileNotFoundException.h"
+#include "ColorFileFormatException.h"
+#include "CoordinateOutOfRangeException.h"
+#include "ColorVectorOutOfRangeException.h"
 
-int main()
-{
+using namespace std;
 
+int main() {
+    ifstream infile("color.txt");
 
+    if (!infile) {
+        throw FIleNotFoundException();
+    }
 
+    Picture pic(5, 5);
+
+    int x, y, r, g, b;
+ 
 }
 
