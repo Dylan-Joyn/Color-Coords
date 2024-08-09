815de899f712c5960a423354c1d25c411aa1084e
Dylan-Joyn <jdjjoyner@gmail.com>
2024-08-09 05:45:43 -0500

    final

--- Color Coords/Color Coords.cpp
+++ Color Coords/Color Coords.cpp
@@ -23,6 +23,35 @@ int main() {
     Picture pic(5, 5);
 
     int x, y, r, g, b;
- 
+    while (infile >> x >> y >> r >> g >> b) {
+        if (infile.fail()) {
+            throw ColorFileFormatException();
+        }
+        try {
+            if (x < 0 || y < 0) {
+                throw CoordinateOutOfRangeException();
+            }
+
+            Color color(r, g, b);
+            if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255) {
+                string message = "Error! X: " + to_string(x) + " Y: " + to_string(y);
+                if (r < 0 || r > 255) message += " Red value out of field: " + to_string(r);
+                if (g < 0 || g > 255) message += " Green value out of field: " + to_string(g);
+                if (b < 0 || b > 255) message += " Blue value out of field: " + to_string(b);
+                throw ColorVectorOutOfRangeException(message);
+            }
+
+            pic.setPixel(x, y, color);
+        }
+        catch (const ColorVectorOutOfRangeException& e) {
+            cout << e.what() << endl;
+        }
+        catch (const CoordinateOutOfRangeException& e) {
+            cout << e.what() << endl;
+        }
+    }
+
+    cout << pic;
+
 }
 
